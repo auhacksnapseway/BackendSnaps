b692c102e25932e3efc4c2e6559210b0cd3dedd6
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-04-06 12:00:10 +0200

    Better api

--- backendsnaps/serializers.py
+++ backendsnaps/serializers.py
@@ -8,7 +8,7 @@ from .models import Event, DrinkEvent
 User = get_user_model()
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ('id', 'username', 'events', 'drink_events', 'password')
@@ -24,30 +24,19 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 		return user
 
 
-class EventSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Event
-		fields = ('id', 'start_datetime', 'end_datetime', 'name', 'owner', 'users')
-		read_only_fields = ('end_datetime', 'owner', 'users')
-
-
-class DrinkEventSerializer(serializers.HyperlinkedModelSerializer):
+class DrinkEventSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = DrinkEvent
 		fields = ('id', 'user', 'event', 'datetime')
 
 
-class CreateUserSerializer(serializers.Serializer):
-	class Meta:
-		model = User
-		fields = ('username', 'password')
-		extra_kwargs = {'password': {'write_only': True}}
+class EventSerializer(serializers.ModelSerializer):
+	drink_events = DrinkEventSerializer(many=True, read_only=True)
 
-	def create(self, validated_data):
-		user = User(username=validated_data['username'])
-		user.set_password(validated_data['password'])
-		user.save()
-		return user
+	class Meta:
+		model = Event
+		fields = ('id', 'start_datetime', 'end_datetime', 'name', 'owner', 'users', 'drink_events')
+		read_only_fields = ('end_datetime', 'owner', 'users')
 
 
 class CreateEventSerializer(serializers.ModelSerializer):
