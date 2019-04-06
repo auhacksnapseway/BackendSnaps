e37fa7468805eebedb569323e6a8219a68954367
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-04-07 00:36:04 +0200

    Add new domain to allowed

--- settings_prod.py
+++ settings_prod.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['api.schnappsapp.com']
+ALLOWED_HOSTS = ['api.schnappsapp.com', 'snaps-api.dropud.nu']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
