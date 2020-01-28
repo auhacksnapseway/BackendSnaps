0bb3a1ca17c10f60765268918e3d7f5597248f6e
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-01-28 17:30:34 +0100

    Add logging to production server

--- settings_prod.py
+++ settings_prod.py
@@ -23,3 +23,41 @@ DATABASES = {
         "PORT": 5432,
     }
 }
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "django.drewsen@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+
+ADMINS = [("Asger Hautop Drewsen", "asgerdrewsen@gmail.com")]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        # Include the default Django email handler for errors
+        # This is what you'd get without configuring logging at all.
+        "mail_admins": {
+            "class": "django.utils.log.AdminEmailHandler",
+            "level": "ERROR",
+            # But the emails are plain text by default - HTML is nicer
+            "include_html": True,
+        },
+        # Log to a text file that can be rotated by logrotate
+        "logfile": {
+            "class": "logging.handlers.WatchedFileHandler",
+            "filename": "/app/django.log",
+        },
+    },
+    "loggers": {
+        # Again, default Django configuration to email unhandled exceptions
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        # Might as well log any errors anywhere else in Django
+        "django": {"handlers": ["logfile"], "level": "ERROR", "propagate": False},
+    },
+}
